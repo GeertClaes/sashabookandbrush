5aae09a915817273be4e4dea318ef3332846f5ad
GeertClaes <geert.wl.claes@gmail.com>
2026-09-08 21:51:53 +0200

    Add sitemap and index support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://sashabookandbrush.com",
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/admin"),
+      namespaces: { news: false, xhtml: false, video: false },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
